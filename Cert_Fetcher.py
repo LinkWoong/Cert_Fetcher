d8dbbb44a5d3cdc1c74632990f257142b9b0c427
LinkWoong <zuiaineo@foxmail.com>
2020-08-29 19:26:31 -0400

    Add cert_fetcher

--- Cert_Fetcher.py
+++ Cert_Fetcher.py
@@ -11,7 +11,14 @@ from bs4 import BeautifulSoup
 from dateutil.parser import parse
 
 def main():
+    parser = argparse.ArgumentParser(description="Arg parser for running this crawler")
+    parser.add_argument('-d', dest='domain', action='store', help="domain name")
+    parser.add_argument('-s', dest='save', action='store', help='destination directory for saving the certificates')
     
+    args = parser.parse_args()
+    cralwer = crtsh_fetcher()
+    certs = []
+    certs_detail = {}
 
 if __name__ == "__main__":
     main()
