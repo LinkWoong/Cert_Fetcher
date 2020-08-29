ad3179929877bbac6003d17d6e642401d60fb906
LinkWoong <zuiaineo@foxmail.com>
2020-08-29 19:22:27 -0400

    Add cert_fetcher

---
+++ Cert_Fetcher.py
@@ -0,0 +1,17 @@
+import requests
+import json
+import argparse
+import os
+import lxml
+import datetime
+import crtsh_fetcher
+import Facebook_fetcher
+
+from bs4 import BeautifulSoup
+from dateutil.parser import parse
+
+def main():
+    
+
+if __name__ == "__main__":
+    main()
